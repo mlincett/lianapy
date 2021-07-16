22b80ec64fa05955b563605c8e873ac1ac58ebd2
Massimiliano Lincetto <m.lincetto@gmail.com>
2021-07-16 18:52:47 +0200

    more histogram tools

--- lianapy/histograms.py
+++ lianapy/histograms.py
@@ -3,11 +3,26 @@ Auxiliary functions to histogram a discrete set of values.
 Bins are centered on integers.
 '''
 
-def hist_discrete_aux(sample):
-    lo, hi = np.min(sample), np.max(sample)
+def hist_discrete_template(lo, hi):
     r = (lo -0.5, hi +0.5)
     b = 1 + hi - lo
     return { 'range': r, 'bins': b }
 
-def hist_discrete(ax, sample):
-    return ax.hist(sample, **hist_discrete_aux(sample))
+def hist_discrete_template_builder(sample):
+    lo, hi = np.min(sample), np.max(sample)
+    return hist_discrete_template(lo, hi)
+    
+def hist_discrete(data, template = None):
+    if template is None:
+        template = hist_discrete_template_builder(sample)
+    return np.histogram(data, **template)
+    
+def hist_discrete_draw(ax, data):
+    return ax.hist(data, **hist_discrete_template_builder(data))
+
+def get_centers(edges):
+    return (edges[1:] + edges[:-1]) / 2
+
+def hist_to_graph(H):
+    values, edges = H[0], H[1]
+    return values, get_centers(edges)
